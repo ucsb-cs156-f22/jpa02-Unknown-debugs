913966cc0d88882ecc9274af77075dde956072bf
Junhwan Lee <junhwanlee2316@gmail.com>
2022-10-11 17:30:08 -0700

    Update HelloController.java

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from junhwanlee";
+        return "Greetings from junhwanlee!";
     }
     
 }
